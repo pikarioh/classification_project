8fd13ed14c0aff80200dd6216ec5af1670f1dd8c
EirinF <56736352+EirinF@users.noreply.github.com>
2020-03-30 22:56:56 +0200

    added project files

---
+++ GetData.m
@@ -0,0 +1,9 @@
+function [data,identifier] = GetData()
+%GETDATA Summary: Loads the .dat file and outputs the data table along with
+%rowheader as identifier
+
+data = textscan('vowdata_nohead.dat','U16');
+identifier = data(1:1668,0);
+data = data(1:1668,7:16);
+end
+
